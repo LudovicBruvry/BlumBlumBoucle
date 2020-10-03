b5b553578475b50e4df0b5b7dba1d89e60f61129
LAPTOP-3B1T551C\Ludovic <l.bruvry@waigeo.fr>
2020-10-04 01:57:34 +0200

    J'ai commitaient

--- Levels/level2.js
+++ Levels/level2.js
@@ -1,11 +1,29 @@
 const planets = [
-    { x: 300, y: 400, size: 80, color: '#F0F', orbitDistance: 50 },
-    { x: 600, y: 700, size: 60, color: '#0FF', orbitDistance: 50 },
-    /*
-    { x: 600, y: 200, size: 60, color: '#0FF', orbitDistance: 50 },
-    { x: 200, y: 100, size: 15, color: '#FF0', orbitDistance: 50 },
-    { x: 200, y: 700, size: 15, color: '#FF0', orbitDistance: 50 },
-    */
+    { x: 100, y: 450, size: 15, color: '#F0F', orbitDistance: 50 },
+    { x: 100, y: 575, size: 15, color: '#0FF', orbitDistance: 50 },
+    { x: 100, y: 700, size: 15, color: '#0FF', orbitDistance: 50 },
+    { x: 100, y: 825, size: 15, color: '#FF0', orbitDistance: 50 },
+    { x: 100, y: 100, size: 15, color: '#FF0', orbitDistance: 50 },
+    { x: 450, y: 220, size: 350, color: '#FF0', orbitDistance: 50 },
+    { x: 800, y: 50, size: 30, color: '#FF0', orbitDistance: 50 },
+    { x: 1250, y: 125, size: 150, color: '#FF0', orbitDistance: 50 },
+    { x: 1250, y: 400, size: 200, color: '#FF0', orbitDistance: 50 },
+    { x: 775, y: 350, size: 200, color: '#FF0', orbitDistance: 50 },
+    { x: 1800, y: 75, size: 15, color: '#FF0', orbitDistance: 50 },
+    { x: 1800, y: 200, size: 15, color: '#FF0', orbitDistance: 50 },
+    { x: 1800, y: 325, size: 15, color: '#FF0', orbitDistance: 50 },
+    { x: 1850, y: 450, size: 15, color: '#F0F', orbitDistance: 50 },
+    { x: 1825, y: 825, size: 60, color: '#FF0', orbitDistance: 50 },
+    { x: 1600, y: 825, size: 15, color: '#FF0', orbitDistance: 50 },
+    { x: 1450, y: 700, size: 15, color: '#FF0', orbitDistance: 50 },
+    { x: 1300, y: 575, size: 15, color: '#FF0', orbitDistance: 50 },
+    { x: 900, y: 575, size: 15, color: '#FF0', orbitDistance: 50 },
+    { x: 500, y: 500, size: 10, color: '#FF0', orbitDistance: 30 },
+    { x: 300, y: 825, size: 15, color: '#FF0', orbitDistance: 50 },
+    { x: 500, y: 750, size: 150, color: '#FF0', orbitDistance: 50 },
+    { x: 700, y: 650, size: 150, color: '#FF0', orbitDistance: 50 },
+  
+  
   ];
 
   export {planets}
