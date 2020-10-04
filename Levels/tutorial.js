ec61128629d2b3ba26caefa70d0ed672344f6537
LAPTOP-3B1T551C\Ludovic <l.bruvry@waigeo.fr>
2020-10-04 17:55:41 +0200

    no message

--- Levels/tutorial.js
+++ Levels/tutorial.js
@@ -1,13 +1,13 @@
 const level = {
     planets : [
-    { x: 100, y: 400, size: 80, image: 'earth', orbitDistance: 50 },
-    { x: 300, y: 300, size: 20, image: 'saumon', orbitDistance: 50 },
-    { x: 500, y: 500, size: 20, image: 'crateres', orbitDistance: 50 },
-    { x: 800, y: 400, size: 30, image: 'crateres', orbitDistance: 50 },
+    { x: 100, y: 400, size: 90, image: 'earth', orbitDistance: 50 },
+    { x: 300, y: 300, size: 30, image: 'saumon', orbitDistance: 50 },
+    { x: 500, y: 500, size: 30, image: 'crateres', orbitDistance: 50 },
+    { x: 800, y: 400, size: 40, image: 'crateres', orbitDistance: 50 },
     { x: 1000, y: 450, size: 100, image: 'saumon', orbitDistance: 50 },
-    { x: 1200, y: 200, size: 20, image: 'saumon', orbitDistance: 50 },
-    { x: 1400, y: 400, size: 80, image: 'crateres', orbitDistance: 50 },
-    { x: 1800, y: 400, size: 50, image: 'earth', orbitDistance: 50 }
+    { x: 1200, y: 200, size: 30, image: 'saumon', orbitDistance: 50 },
+    { x: 1400, y: 400, size: 90, image: 'crateres', orbitDistance: 50 },
+    { x: 1800, y: 400, size: 60, image: 'earth', orbitDistance: 50 }
     ],
     background : 'Assets/Sprites/Fond4.jpg',
     music : 'Assets/Music/LEVEL1_2.mp3',
