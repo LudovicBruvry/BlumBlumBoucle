a4936c3fae733d809c83d873b826f60619fa5434
LAPTOP-3B1T551C\Ludovic <l.bruvry@waigeo.fr>
2020-10-04 17:46:47 +0200

    no message

--- Levels/tutorial.js
+++ Levels/tutorial.js
@@ -6,8 +6,8 @@ const level = {
     { x: 800, y: 400, size: 30, image: 'crateres', orbitDistance: 50 },
     { x: 1000, y: 450, size: 100, image: 'saumon', orbitDistance: 50 },
     { x: 1200, y: 200, size: 20, image: 'saumon', orbitDistance: 50 },
-    { x: 1250, y: 400, size: 80, image: 'crateres', orbitDistance: 50 },
-    { x: 1500, y: 400, size: 20, image: 'earth', orbitDistance: 50 }
+    { x: 1400, y: 400, size: 80, image: 'crateres', orbitDistance: 50 },
+    { x: 1800, y: 400, size: 50, image: 'earth', orbitDistance: 50 }
     ],
     background : 'Assets/Sprites/Fond4.jpg',
     music : 'Assets/Music/LEVEL1_2.mp3',
