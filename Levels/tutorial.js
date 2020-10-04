2d4d70309a10e76595ac36d8e730d01fff5208f5
LAPTOP-3B1T551C\Ludovic <l.bruvry@waigeo.fr>
2020-10-04 17:38:40 +0200

    MAJ LEVEL TUTO

--- Levels/tutorial.js
+++ Levels/tutorial.js
@@ -1,23 +1,36 @@
 const level = {
     planets : [
-    { x: 100, y: 400, size: 80, color: '#F0F', orbitDistance: 50 },
-    { x: 300, y: 300, size: 20, color: '#0FF', orbitDistance: 50 },
-    { x: 500, y: 500, size: 20, color: '#0FF', orbitDistance: 50 },
-    { x: 800, y: 400, size: 30, color: '#0FF', orbitDistance: 50 },
-    { x: 1000, y: 450, size: 100, color: '#0FF', orbitDistance: 50 },
-    { x: 1200, y: 200, size: 20, color: '#0FF', orbitDistance: 50 },
-    { x: 1250, y: 400, size: 80, color: '#0FF', orbitDistance: 50 },
-    { x: 1500, y: 400, size: 20, color: '#0FF', orbitDistance: 50 }
+    { x: 100, y: 400, size: 80, image: 'earth', orbitDistance: 50 },
+    { x: 300, y: 300, size: 20, image: 'saumon', orbitDistance: 50 },
+    { x: 500, y: 500, size: 20, image: 'crateres', orbitDistance: 50 },
+    { x: 800, y: 400, size: 30, image: 'crateres', orbitDistance: 50 },
+    { x: 1000, y: 450, size: 100, image: 'saumon', orbitDistance: 50 },
+    { x: 1200, y: 200, size: 20, image: 'saumon', orbitDistance: 50 },
+    { x: 1250, y: 400, size: 80, image: 'crateres', orbitDistance: 50 },
+    { x: 1500, y: 400, size: 20, image: 'earth', orbitDistance: 50 }
     ],
     background : 'Assets/Sprites/Fond4.jpg',
     music : 'Assets/Music/LEVEL1_2.mp3',
-    asteroidLines : [
-        { points: [
-            { x: 100, y: 500 },
-            { x: 400, y: 510 },
-            { x: 500, y: 430 },
-            { x: 1200, y: 600 }
-        ] },
+    asteroidLines :
+    [
+      {
+        points: [
+            { x: 500, y: 100 },
+            { x: 800, y: 300 },
+        ],
+      },
+      {
+        points: [
+            { x: 650, y: 600 },
+            { x: 850, y: 490 },
+        ],
+      },
+      {
+        points: [
+            { x: 1000, y: 350 },
+            { x: 1150, y: 250 },
+        ],
+      },
     ],
 };
 
