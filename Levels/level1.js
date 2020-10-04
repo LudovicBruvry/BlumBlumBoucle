5c3fac3be745b4e2d9647d2c50a63a55677a59e9
LAPTOP-3B1T551C\Ludovic <l.bruvry@waigeo.fr>
2020-10-04 18:19:29 +0200

    LEVEL 1

--- Levels/level1.js
+++ Levels/level1.js
@@ -14,7 +14,7 @@ const level = {
     { x: 1450, y: 350, size: 40, image: 'crateres', orbitDistance: 50 },
     { x: 1480, y: 520, size: 60, image: 'saumon', orbitDistance: 50 },
 
-    { x: 500, y: 460, size: 40, image: 'crateres', orbitDistance: 50 },
+    { x: 850, y: 300, size: 40, image: 'crateres', orbitDistance: 50 },
     { x: 790, y: 440, size: 40, image: 'saumon', orbitDistance: 50 },
     { x: 620, y: 500, size: 40, image: 'earth', orbitDistance: 50 },
 
@@ -23,7 +23,7 @@ const level = {
     { x: 1300, y: 680, size: 80, image: 'saumon', orbitDistance: 50 },
     { x: 1100, y: 660, size: 80, image: 'saumon', orbitDistance: 50 },
 
-    { x: 1500, y: 700, size: 35, image: 'earth', orbitDistance: 50 },
+    { x: 1800, y: 775, size: 60, image: 'earth', orbitDistance: 50 },
   ],
   background: 'Assets/Sprites/Fond5.jpg',
   music: 'Assets/Music/LEVEL1_2.mp3',
@@ -31,9 +31,43 @@ const level = {
     {
       points: [
         { x: 100, y: 500 },
-        { x: 400, y: 510 },
-        { x: 500, y: 430 },
-        { x: 1200, y: 600 },
+        { x: 675, y: 350 },
+      ],
+    },
+    {
+      points: [
+        { x: 750, y: 100 },
+        { x: 1000, y: 350 },
+      ],
+    },
+    {
+      points: [
+        { x: 900, y: 600 },
+        { x: 950, y: 350 },
+      ],
+    },
+    {
+      points: [
+        { x: 1200, y: 200 },
+        { x: 1350, y: 450 },
+      ],
+    },
+    {
+      points: [
+        { x: 1000, y: 550 },
+        { x: 1300, y: 450 },
+      ],
+    },
+    {
+      points: [
+        { x: 1550, y: 120 },
+        { x: 1750, y: 700 },
+      ],
+    },
+    {
+      points: [
+        { x: 100, y: 25 },
+        { x: 350, y: 125 },
       ],
     },
   ],
